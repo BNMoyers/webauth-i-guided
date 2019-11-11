6350bd067edf51079d96043efff6678bfc0457ab
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2019-11-11 11:25:20 -0600

    checkpoint 1

---
+++ api/api-router.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+
+const authRouter = require('../auth/auth-router.js');
+const usersRouter = require('../users/users-router.js');
+
+router.use('/auth', authRouter);
+router.use('/users', usersRouter);
+
+router.get('/', (req, res) => {
+  res.json({ api: "It's alive" });
+});
+
+router.post('/hash', (req, res) => {
+  //read a password from the body
+  //hash the password
+  //return it to the user in an object that looks like:
+  // { password: 'original password', hash: 'hashed password' }
+})
+
+module.exports = router;
